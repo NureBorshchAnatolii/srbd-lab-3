b383f358622639c4b5d9516d4bb03329f4e4c219
Anatolii-Borshch <anatoliiborshch@gmail.com>
2025-11-25 14:36:30 +0200

    [feat] added entity for table function

---
+++ GameMovieStore/Dtos/PurchasePerDay.cs
@@ -0,0 +1,8 @@
+﻿namespace GameMovieStore.Dtos
+{
+    public class PurchasePerDay
+    {
+        public DateTime PurchaseDate { get; set; }
+        public int PurchaseCount { get; set; }
+    }
+}
